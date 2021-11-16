ae876fef39d6bb7fd604cc88928b073d0ab7b0ba
gundyn <nathan.gundy@gmail.com>
2021-11-15 18:21:29 -0600

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import { render } from 'react-dom'
 
-function App() {
+// Import Components
+import TodoForm from './Components/ToDoForm'
+import ToDoList from './Components/ToDoList'
+
+// Import Interfaces 
+import { TodoInterface } from './interface'
+// import './style.css'
+
+const App: React.FC = () => {
+  const [todos, setTodos] = React.useState<TodoInterface[]>([])
+  // creating new todo items
+  function handleTodoCreate(todo: TodoInterface) {
+    const newTodosState: TodoInterface[] = [...todos]
+    newTodosState.push(todo)
+    setTodos(newTodosState)
+  }
+  // Update existing todo item 
+  function handleTodoUpdate(event: React.ChangeEvent<HTMLInputElement>, id: string) {
+    const newTodosState: TodoInterface[] = [...todos]
+    
+    newTodosState.find((todo: TodoInterface) => todo.id === id)!.name = event.target.value
+    
+    setTodos(newTodosState)
+  }
+  
+  // remove existing todo item
+  function handleTodoRemove(id: string) {
+    const newTodosState: TodoInterface[] = todos.filter((todo: TodoInterface) => todo.id !== id)
+    
+    setTodos(newTodosState)
+  }
+  
+  // Check existing todo item as completed 
+  function handleTodoComplete(id: string) {
+    const newTodosState: TodoInterface[] = [...todos]
+    // Find the correct todo item and update its 'isCompleted' key 
+    newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted = !newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted
+    
+    setTodos(newTodosState)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <h2>My ToDo APP</h2>
+        <TodoForm 
+          todos={todos}
+          handleTodoUpdate={handleTodoUpdate}
+          handleTodoRemove={handleTodoRemove}
+          handleTodoComplete={handleTodoComplete}
+        />
+      </React.Fragment>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/interface.ts
@@ -0,0 +1,29 @@
+// For Todo interface
+export interface TodoInterface {
+  id: string
+  name: string
+  isCompleted: boolean
+}
+
+// For Todo form interface
+export interface TodoFormInterface {
+  todos: TodoInterface[]
+  handleTodoCreate: (todo: TodoInterface) => void 
+  //return type is void
+}
+
+// For Todo list interface
+export interface TodoListInterface {
+  handleTodoUpdate: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void
+  handleTodoRemove: (id: string) => void
+  handleTodoComplete: (id: string) => void
+  todos: TodoInterface[]
+}
+
+// For Todo item interface 
+export interface TodoItemInterface {
+  handleTodoUpdate: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void
+  handleTodoRemove: (id: string) => void
+  handleTodoComplete: (id: string) => void
+  todo: TodoInterface
+}
